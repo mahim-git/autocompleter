b4c2960ee279e426b7cc74706f22f2f7f10dc4c9
mahim-git <72382970+mahim-git@users.noreply.github.com>
2020-11-19 01:08:16 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const text = `Allah is almighty`
+let index = 0;
+function writeText(){
+    document.body.innerText = text.slice(0, index);
+    index++;
+    
+    if(index>text.length){
+        index = 0;
+    }
+};
+
+setInterval(writeText,60)
